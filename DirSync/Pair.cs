5bfc5a16038a4f295ae0b9ae457e34dc1b9f5d4e
Ahmet Sait <git@ahmetsait.com>
2016-07-23 03:49:03 +0300

    Initial commit

---
+++ DirSync/Pair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirSync
+{
+	public struct Pair<T>
+	{
+		public T Item1, Item2;
+
+		public Pair(T item1, T item2)
+		{
+			this.Item1 = item1;
+			this.Item2 = item2;
+		}
+	}
+}
---
+++ DirSync/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirSync
+{
+	internal sealed class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			//Application.CurrentCulture = CultureInfo.InvariantCulture;
+			Application.Run(new MainForm());
+		}
+
+		public static string exeDir = Path.GetDirectoryName(Application.ExecutablePath),
+			configPath = Path.Combine(exeDir, "path.ini");
+	}
+}
