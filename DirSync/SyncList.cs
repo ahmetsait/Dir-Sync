55a1ff601cae29b3b220a8b51d1824834c73c6d7
Ahmet Sait <git@ahmetsait.com>
2017-11-23 03:31:14 +0300

    Improved Diff (Sync) algorithm
    Improved performance by removing file comparison
    Last write error margin lowered to 1 minute
    Better async cancellation support
    Relevancy handling of mirrors

--- DirSync/SyncList.cs
+++ DirSync/SyncList.cs
@@ -5,13 +5,13 @@ namespace DirSync
 {
 	public class SyncList
 	{
-		public IList<Pair<FileInfo>> filePairs;
 		public IList<Pair<DirectoryInfo>> dirPairs;
+		public IList<Pair<FileInfo>> filePairs;
 		
 		public SyncList()
 		{
-			filePairs = new List<Pair<FileInfo>>(256);
 			dirPairs = new List<Pair<DirectoryInfo>>(64);
+			filePairs = new List<Pair<FileInfo>>(256);
 		}
 	}
 }
